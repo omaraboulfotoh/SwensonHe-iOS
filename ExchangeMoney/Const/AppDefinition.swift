6b82074f9476b5ebe50c8b25aa0c90a3f9cead82
omar aboulfotoh <omar.aboulfotoh@fixedmea.com>
2021-06-29 06:28:34 +0200

    add application core and init list item

---
+++ ExchangeMoney/Const/AppDefinition.swift
@@ -0,0 +1,12 @@
+//
+//  AppDefinition.swift
+//  ExchangeMoney
+//
+//  Created by User on 29/06/2021.
+//
+
+import Foundation
+
+let BASE_URL: String = "http://data.fixer.io/api/"
+let API_KEY = "3036a78c565ef28cad0928b992c0fcca"
+let DEFAULT_CURRENCY = "USA"
---
+++ ExchangeMoney/Modules/View/Main/CurrencyItem/CurrencyTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CurrencyTableViewCell.swift
+//  ExchangeMoney
+//
+//  Created by User on 29/06/2021.
+//
+
+import UIKit
+
+class CurrencyTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
